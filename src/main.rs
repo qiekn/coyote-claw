76d9a4d1257c49b82994a7be24ad9b43db46df7a
qiekn <qiekndev@gmail.com>
2026-03-30 09:09:40 +0800

    demo: unwrap() & expect()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
   println!("Hello, world!");
+
+  // ----------------------------------------------------------------------------: unwrap & expect
+
+  let x: Result<i32, &str> = Err("x error");
+  let y: Result<i32, &str> = Ok(5);
+  let value = y.unwrap(); // value = 5;
+
+  let _ = x.expect("qiekn expect value:");
+  let _ = x.unwrap();
+
+  println!("value: {}", value);
 }
